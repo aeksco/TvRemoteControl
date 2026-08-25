1bf80255cae83802bdac1bf1f90fe95c14f7d169
Alexander Schwartzberg <aeksco@gmail.com>
2026-08-25 00:05:45 -0400

    Initial Commit

---
+++ TvRemoteControl/TvRemoteControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  TvRemoteControlApp.swift
+//  TvRemoteControl
+//
+//  Created by Alexander Schwartzberg on 8/25/26.
+//
+
+import SwiftUI
+
+@main
+struct TvRemoteControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
